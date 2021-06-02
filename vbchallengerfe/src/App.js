e8ce39af24194fabfe0913f8aac2ceb545b5baa6
Brian Kubes <briankubes@gmail.com>
2021-06-02 12:30:30 -0700

    added Home.js

--- vbchallengerfe/src/App.js
+++ vbchallengerfe/src/App.js
@@ -1,46 +1,11 @@
 import "./App.css";
-import { io } from "socket.io-client";
-import React, { useState, useEffect } from "react";
+import Home from "./components/Home.js";
 
 function App() {
-    const socket = io("http://localhost:4000");
-    // const productSocket = io("https://example.com/product", { forceNew: true }); // the "product" namespace
-
-    // This is receiving messages from the BE labeled "connection"
-    useEffect(() => {
-        socket.on("connection", (data) => {
-            console.log("Connection from FE", data);
-            socket.on("message", (data) => {
-                document.querySelector("h2").innerHTML = data;
-            });
-        });
-    }, []);
-
-    const [greeting, setGreeting] = useState("");
-
-    const handleChange = (e) => {
-        setGreeting(e.target.value);
-    };
-
-    // This sends a message to the backend labeled "message"
-    const handleSubmit = (e) => {
-        // e.preventDefaut();
-        socket.emit("message", greeting);
-    };
-
     return (
-        <div className="App">
-            <form onSubmit={handleSubmit}>
-                <input
-                    type="text"
-                    name="greeting"
-                    value={greeting}
-                    onChange={handleChange}
-                ></input>
-                <input type="submit"></input>
-            </form>
-            <h2>temp</h2>
-        </div>
+        <>
+            <Home></Home>
+        </>
     );
 }
 
