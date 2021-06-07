b83b3c6f02155bf99646c67341a2fade0b937873
Brian Kubes <briankubes@gmail.com>
2021-06-07 16:12:43 -0700

    fixed typo and removed React.StrictMode from index.js

--- vbchallengerfe/src/components/Home.js
+++ vbchallengerfe/src/components/Home.js
@@ -3,6 +3,12 @@ import { io } from "socket.io-client";
 import "../App.css";
 
 export default function Home() {
+    const [passcode, setPasscode] = useState({
+        code: "",
+    });
+
+    const [waitlist, setWaitlist] = useState([]);
+
     const socket = io("http://localhost:4000");
     // const productSocket = io("https://example.com/product", { forceNew: true }); // the "product" namespace
 
@@ -10,37 +16,49 @@ export default function Home() {
     useEffect(() => {
         socket.on("connection", (data) => {
             console.log("Connection from FE:", data);
-            socket.on("message", (data) => {
-                document.querySelector("h2").innerHTML = data;
-            });
+            // socket.emit("sendPasscode", passcode);
+        });
+        socket.on("message", (data) => {
+            console.log("FE receiving from BE in message:", data.code);
+            // document.querySelector("h2").innerHTML = data.code;
+            setWaitlist(data.code);
+        });
+        return () => socket.disconnect();
+    }, [socket]);
+
+    useEffect(() => {
+        socket.on("disconnect", () => {
+            socket.removeAllListeners();
         });
     }, []);
 
-    const [greeting, setGreeting] = useState("");
-
     const handleChange = (e) => {
-        setGreeting(e.target.value);
+        setPasscode({ code: e.target.value });
     };
 
     // This sends a message to the backend labeled "message"
     const handleSubmit = (e) => {
-        // e.preventDefaut();
-        socket.emit("message", greeting);
+        e.preventDefault();
+        // localStorage.setItem("passcode", passcode.code);
+        // setWaitlist(passcode.code);
+        socket.emit("sendPasscode", passcode);
     };
 
     return (
         <>
             <div className="App">
                 <form onSubmit={handleSubmit}>
+                    <label htmlFor="passcode">Beach Passcode</label>
                     <input
                         type="text"
-                        name="greeting"
-                        value={greeting}
+                        name="passcode"
+                        value={passcode.code}
                         onChange={handleChange}
                     ></input>
                     <input type="submit"></input>
                 </form>
                 <h2>temp</h2>
+                <h3>{waitlist}</h3>
             </div>
         </>
     );
